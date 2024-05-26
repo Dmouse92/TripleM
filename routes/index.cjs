18f1173e4d68167cd99861bee8cb05c7362898aa
KooshaPari <kooshapari@gmail.com>
2024-05-26 15:03:26 -0700

    Vercel Implementation 4

--- routes/index.cjs
+++ routes/index.cjs
@@ -3,7 +3,7 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
 	console.log('Request for index.html received')
-	res.sendFile('index.html', { root: 'public' }, (err) => {
+	res.sendFile(path.join(__dirname, '../public/index.html'), { root: 'public' }, (err) => {
 		if (err) {
 			console.error('Error serving index.html:', err)
 			res.status(404).send('File not found')
